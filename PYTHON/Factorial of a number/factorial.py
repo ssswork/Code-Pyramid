9437495bf92f8b17f40e2fd12010771e67c17ce9
ssswork <121511031+ssswork@users.noreply.github.com>
2022-12-27 23:10:50 +0530

    Added Comments
    Added some comments to the program so that the beginner's can understand it very well without any problem, if you like it then you can merge this commit, Thank you.

--- PYTHON/Factorial of a number/factorial.py
+++ PYTHON/Factorial of a number/factorial.py
@@ -1,8 +1,12 @@
+#Getting a input of the number from the user
 num=int(input("Enter a number: "))
+#Creating a variable for the factorial value
 fact=1
 a=1
 
+#Code for getting the factorial of the number
 while a <= num:
     fact*= a
     a+=1
+#Printing the factorial value
 print("The factorial of", num, "is", fact)
